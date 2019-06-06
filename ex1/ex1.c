c3eae133f8e2099f3dce8b0ff08894f86be0c80d
matt0418 <mfeldma7@gmail.com>
2019-06-06 16:00:22 -0400

    completed exercises 1-2

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,43 @@
 int main(void)
 {
     // Your code here
+    int x = 10;
+    printf("Parent ID is %d\n", (int) getpid());
+    int forky = fork();
+    if (forky == 0) {
+        printf("This is the child process and this is my ID: %d\n", (int) getpid());
+        printf("This is the value of X from before %d\n", x);
+        x *= 2;
+        printf("Now this is the value of X %d\n", x);
+    } else {
+        printf("This is the parent process");
+        printf("This is the current value of X %d\n", x);
+        x++;
+        printf("This is the value of X now %d\n", x);
+    }
 
     return 0;
 }
+
+// int main(void)
+// {
+//     int x = 100;
+//     printf("hello world (pid: %d)\n", (int) getpid());
+
+//     int rc = fork();
+
+//     if (rc < 0) {
+//         fprintf(stderr, "fork failed\n");
+//         exit(1);
+//     } else if (rc == 0) {
+//         printf("hello, I am child (pid: %d) and x is: %d\n", (int) getpid(), x);
+//         x++;
+//         printf("child again, x is now: %d\n", x);
+//     } else {
+//         printf("hello, I am parent of %d (pid: %d) and x is: %d\n", rc, (int) getpid(), x);
+//         x--;
+//         printf("parent again, x is now: %d\n", x);
+//     }
+
+//     return 0;
+// }
--- ex2/ex2.c
+++ ex2/ex2.c
@@ -5,10 +5,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
     // Your code here 
-    
+    FILE *opened;
+    opened = fopen("text.txt", "w");
+    int thing = fork();
+
+    if (thing == 0) {
+        printf("Child");
+        char *stringy = "Child Hello There";
+        fwrite(stringy, sizeof(char), strlen(stringy), opened);
+    } else {
+        printf("Parent");
+        char *adult_string = "Parent Hello There";
+        fwrite(adult_string, sizeof(char), strlen(adult_string), opened);
+    }
+    fclose(opened);
     return 0;
 }
